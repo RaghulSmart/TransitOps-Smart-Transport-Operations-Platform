442c4fcbf403cb60d6224524079f07104f41a968
Raghul <raghulsmart007@gmail.com>
2026-07-12 17:20:51 +0530

    Dashboard Updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -85,9 +85,74 @@ exports.login = async (req, res) => {
 
 // Dashboard
 exports.dashboard = async (req, res) => {
+    const [vehicles] = await db.query(
+        "SELECT status, count(status) overview FROM vehicles GROUP BY status"
+    );
+    const [drivers] = await db.query(
+        "SELECT status, count(status) overview FROM drivers GROUP BY status"
+    );
+    const [trips] = await db.query(
+        "SELECT status, count(status) overview FROM trips GROUP BY status"
+    );
+    const display_vehicle_status = {
+        "available" : {
+            "class" : "success",
+            "text" : "Available"
+        },
+        "retired" : {
+            "class" : "danger",
+            "text" : "Retired"
+        },
+        "in_shop" : {
+            "class" : "info",
+            "text" : "In Shop"
+        },
+        "on_trip" : {
+            "class" : "warning",
+            "text" : "On Trip"
+        }
+    };
+    const display_driver_status = {
+        "available" : {
+            "class" : "success",
+            "text" : "Available"
+        },
+        "suspended" : {
+            "class" : "danger",
+            "text" : "Suspended"
+        },
+        "off_duty" : {
+            "class" : "info",
+            "text" : "Off Duty"
+        },
+        "on_trip" : {
+            "class" : "warning",
+            "text" : "On Trip"
+        }
+    };
+    const display_trip_status = {
+        "dispatched" : {
+            "class" : "info",
+            "text" : "Dispatched"
+        },
+        "cancelled" : {
+            "class" : "danger",
+            "text" : "Cancelled"
+        },
+        "completed" : {
+            "class" : "success",
+            "text" : "Completed"
+        }
+    };
     try {
         res.render("dashboard/index", {
-             user: req.session.user
+            display_vehicle_status,
+            display_driver_status,
+            display_trip_status,
+            drivers,
+            vehicles,
+            trips,
+            user: req.session.user
         });
     } catch (err) {
         console.log(err);
